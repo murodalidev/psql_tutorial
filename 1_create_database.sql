79ac16ce050a6be2ee663286df27a1e46cfcf530
murodalidev <murodalinarzullaevofficial@gmail.com>
2023-02-22 02:19:34 +0500

    modified

---
+++ 1_create_database.sql
@@ -0,0 +1,8 @@
+
+-- Tezroq va sodda yo'l bilan database yaratish
+CREATE DATABASE school;
+
+
+
+-- Qo'shimcha ma'lumotlar bilan qo'shib yaratish
+CREATE DATABASE database_name WITH OWNER = psql_user ENCODING = 'UTF-8' CONNECTION LIMIT = -1;
